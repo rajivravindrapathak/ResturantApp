d2218e87f47de2bab290f0b227bd0cf63e0cfacb
Rajiv Ravindra pathak <70104100+rajivravindrapathak@users.noreply.github.com>
2022-10-31 10:58:30 +0530

    Add files via upload

---
+++ assignment/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Restorant from './Components/Restorant';
+
+function App() {
+  return (
+    <div className="App">
+     <Restorant />
+    </div>
+  );
+}
+
+export default App;
---
+++ assignment/src/Components/RestaurantDetails.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const RestaurantDetails = ({url, title, cost, rating, votes, reviews, tags}) => {
+    
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default RestaurantDetails
